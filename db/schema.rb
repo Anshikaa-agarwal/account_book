822274f2f15a91f5a8dccc00d7d851c10e605673
Anshika Agarwal <anshika2020ag@gmail.com>
2026-03-20 13:48:10 +0530

    bundle install/db:migrate

---
+++ db/schema.rb
@@ -0,0 +1,67 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[8.1].define(version: 2026_03_19_141152) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.decimal "balance", precision: 10, scale: 3, default: "0.0", null: false
+    t.bigint "bank_id", null: false
+    t.datetime "created_at", null: false
+    t.string "number"
+    t.integer "type", default: 0, null: false
+    t.datetime "updated_at", null: false
+    t.index ["bank_id"], name: "index_accounts_on_bank_id"
+  end
+
+  create_table "banks", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_banks_on_user_id"
+  end
+
+  create_table "titles", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_titles_on_user_id"
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.bigint "account_id", null: false
+    t.decimal "amount", precision: 8, scale: 3, null: false
+    t.decimal "balance", precision: 10, scale: 3, default: "0.0", null: false
+    t.datetime "created_at", null: false
+    t.datetime "date_time", null: false
+    t.bigint "title_id"
+    t.integer "type", null: false
+    t.datetime "updated_at", null: false
+    t.index ["account_id"], name: "index_transactions_on_account_id"
+    t.index ["title_id"], name: "index_transactions_on_title_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "accounts", "banks"
+  add_foreign_key "banks", "users"
+  add_foreign_key "titles", "users"
+  add_foreign_key "transactions", "accounts"
+  add_foreign_key "transactions", "titles"
+end
