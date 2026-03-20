45f6456fb7f21d808e0f7e836ac230ddfc1ba721
Anshika Agarwal <anshika2020ag@gmail.com>
2026-03-20 13:28:13 +0530

    add balance to transactions and accounts

--- db/migrate/20260319141152_create_transactions.rb
+++ db/migrate/20260319141152_create_transactions.rb
@@ -2,9 +2,10 @@ class CreateTransactions < ActiveRecord::Migration[8.1]
   def change
     create_table :transactions do |t|
       t.references :title, foreign_key: true
-      t.date :date
-      t.decimal :amount, precision: 8, scale: 3
-      t.integer :type, null: false
+      t.datetime   :date_time
+      t.decimal    :amount,  precision: 8, scale: 3
+      t.decimal    :balance, precision: 10, scale: 3
+      t.integer    :type,    null: false
       t.references :account, null: false, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_19_141152) do
   enable_extension "pg_catalog.plpgsql"
 
   create_table "accounts", force: :cascade do |t|
+    t.decimal "balance", precision: 10, scale: 3
     t.bigint "bank_id", null: false
     t.datetime "created_at", null: false
     t.string "number"
@@ -42,8 +43,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_19_141152) do
   create_table "transactions", force: :cascade do |t|
     t.bigint "account_id", null: false
     t.decimal "amount", precision: 8, scale: 3
+    t.decimal "balance", precision: 10, scale: 3
     t.datetime "created_at", null: false
-    t.date "date"
+    t.datetime "date_time"
     t.bigint "title_id"
     t.integer "type", null: false
     t.datetime "updated_at", null: false
