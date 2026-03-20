88735c35ba83c93d3a5c0a6bf167207cb6b139c4
Anshika Agarwal <anshika2020ag@gmail.com>
2026-03-20 14:36:09 +0530

    use a scale of 2 for decimals

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_19_141152) do
   enable_extension "pg_catalog.plpgsql"
 
   create_table "accounts", force: :cascade do |t|
-    t.decimal "balance", precision: 10, scale: 3, default: "0.0", null: false
+    t.decimal "balance", precision: 10, scale: 2, default: "0.0", null: false
     t.bigint "bank_id", null: false
     t.datetime "created_at", null: false
     t.string "number"
@@ -42,8 +42,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_19_141152) do
 
   create_table "transactions", force: :cascade do |t|
     t.bigint "account_id", null: false
-    t.decimal "amount", precision: 8, scale: 3, null: false
-    t.decimal "balance", precision: 10, scale: 3, default: "0.0", null: false
+    t.decimal "amount", precision: 8, scale: 2, null: false
+    t.decimal "balance", precision: 10, scale: 2, default: "0.0", null: false
     t.datetime "created_at", null: false
     t.datetime "date_time", null: false
     t.bigint "title_id"
