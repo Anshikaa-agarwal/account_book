afcc36b19893782e0a41c079a0baa90e1b7f218c
Anshika Agarwal <anshika2020ag@gmail.com>
2026-03-20 13:35:29 +0530

    date_time can not be null

--- db/migrate/20260319141152_create_transactions.rb
+++ db/migrate/20260319141152_create_transactions.rb
@@ -2,7 +2,7 @@ class CreateTransactions < ActiveRecord::Migration[8.1]
   def change
     create_table :transactions do |t|
       t.references :title, foreign_key: true
-      t.datetime   :date_time
+      t.datetime   :date_time, null: false
       t.decimal    :amount,  precision: 8,  scale: 3, null: false
       t.decimal    :balance, precision: 10, scale: 3, null: false, default: 0
       t.integer    :type,    null: false
